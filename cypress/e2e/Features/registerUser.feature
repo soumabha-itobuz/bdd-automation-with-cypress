426765c19e2c0d414135d75a8e8f9a839be65d4c
zeniaitobuz <zenia@itobuz.com>
2024-08-02 11:35:02 +0530

    feat: register user added

--- cypress/e2e/Features/registerUser.feature
+++ cypress/e2e/Features/registerUser.feature
@@ -7,3 +7,14 @@ Scenario: Register user
     When Verify 'New User Signup!' is visible
     Then Enter name and email address and click on signup button
     When Verify that 'Enter Account Information' is visible
+    Then Fill details: Title, Name, Email, Password, Date of birth
+    Then Select checkbox 'Sign up for our newsletter!'
+    Then Select checkbox 'Receive special offers from our partners!'
+    Then Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+    Then Click 'Create Account button'
+    When Verify that 'ACCOUNT CREATED!' is visible
+    Then Click 'Continue' button
+    When Verify that 'Logged in as username' is visible
+    Then Click 'Delete Account' button
+    When Verify that 'ACCOUNT DELETED!' is visible
+    Then Click 'Continue' button again
--- cypress/e2e/Features/registerUser.ts
+++ cypress/e2e/Features/registerUser.ts
@@ -1,5 +1,7 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { faker } from "@faker-js/faker";
 
+const fullName = faker.person.fullName();
 Given("Automation exercise page is opened", function () {
   cy.visit(Cypress.env("baseURL"));
   cy.wait(2000);
@@ -14,12 +16,63 @@ When("Verify 'New User Signup!' is visible", () => {
   cy.get("h2").eq(2).should("have.text", "New User Signup!");
 });
 Then("Enter name and email address and click on signup button", () => {
-  cy.get('[data-qa="signup-name"]').type("Zenia");
-  cy.get('[data-qa="signup-email"]').type("zenia@itobuz.com");
+  cy.get('[data-qa="signup-name"]').type(fullName);
+  cy.get('[data-qa="signup-email"]').type(faker.internet.email());
   cy.get('[data-qa="signup-button"]').click();
 });
 When("Verify that 'Enter Account Information' is visible", () => {
   cy.get("[class='title text-center']")
     .eq(0)
     .should("have.text", "Enter Account Information");
+});
+Then("Fill details: Title, Name, Email, Password, Date of birth", () => {
+  cy.get('[for="id_gender2"]').click();
+  cy.get('[data-qa="password"]').type("Itobuz#1234");
+  cy.get('[data-qa="days"]').select("10");
+  cy.get('[data-qa="months"]').select("February");
+  cy.get('[data-qa="years"]').select("2002");
+});
+Then("Select checkbox 'Sign up for our newsletter!'", () => {
+  cy.get('[id="newsletter"]').check();
+});
+Then("Select checkbox 'Receive special offers from our partners!'", () => {
+  cy.get('[id="optin"]').check();
+});
+Then(
+  "Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number",
+  () => {
+    cy.get('[data-qa="first_name"]').type("Zenia");
+    cy.get('[data-qa="last_name"]').type("Banerjee");
+    cy.get('[data-qa="company"]').type("Itobuz");
+    cy.get('[data-qa="address"]').type("Kolkata");
+    cy.get('[id="country"]').select("India");
+    cy.get('[data-qa="state"]').type("West Bengal");
+    cy.get('[data-qa="city"]').type("Kolkata");
+    cy.get('[data-qa="zipcode"]').type("712258");
+    cy.get('[data-qa="mobile_number"]').type("9874976543");
+  }
+);
+Then("Click 'Create Account button'", () => {
+  cy.get('[data-qa="create-account"]').click();
+});
+When("Verify that 'ACCOUNT CREATED!' is visible", () => {
+  cy.get("[data-qa='account-created']").should("have.text", "Account Created!");
+});
+Then("Click 'Continue' button", () => {
+  cy.get("[data-qa='continue-button']").click();
+});
+When("Verify that 'Logged in as username' is visible", () => {
+  cy.get("[class='nav navbar-nav'] > li:nth-child(10) > a > b").should(
+    "have.text",
+    fullName
+  );
+});
+Then("Click 'Delete Account' button", () => {
+  cy.get("[href='/delete_account']").click();
+});
+When("Verify that 'ACCOUNT DELETED!' is visible", () => {
+  cy.get('[data-qa="account-deleted"]').should("have.text", "Account Deleted!");
+});
+Then("Click 'Continue' button again", () => {
+  cy.get("[data-qa='continue-button']").click();
 });
