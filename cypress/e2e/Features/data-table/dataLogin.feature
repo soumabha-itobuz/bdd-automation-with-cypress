5602b431aa7c0e2e96205ff7227bc12147c47c76
Soumabha Roy <soumabha@itobuz.com>
2024-08-27 17:49:26 +0530

    fix: code smell for data table

--- cypress/e2e/Features/data-table/dataLogin.feature
+++ cypress/e2e/Features/data-table/dataLogin.feature
@@ -4,8 +4,8 @@ Feature: User Login
     Given I open the login page
     When I fill in the following credentials:
       | username | password  |
-      | user1    | password1 |
-      | user2    | password2 |
-      | user3    | password3 |
+      | soumabha+95@itobuz.com   | Itobuz |
+    #   | user2    | password2 |
+    #   | user3    | password3 |
     Then I should see the dashboard page
 
--- cypress/e2e/Features/data-table/dataLogin.ts
+++ cypress/e2e/Features/data-table/dataLogin.ts
@@ -6,48 +6,20 @@ Given('I open the login page', () => {
 //   cy.visit('/login'); // Adjust the path to your login page
   cy.visit(Cypress.env("baseURL"));
   cy.wait(2000);
+  cy.get(".fa.fa-lock").click();
 });
 
 When('I fill in the following credentials:', (dataTable: DataTable) => {
-  dataTable.hashes().forEach((row) => {
-    cy.get('input[name="username"]').clear().type(row.username);
-    cy.get('input[name="password"]').clear().type(row.password);
-    cy.get('button[type="submit"]').click();
-  });
+//   dataTable.hashes().forEach((row) => {
+    const userData = dataTable.rowsHash();
+    cy.get('[name="email"]').first().click().type(userData.username);
+    cy.get('[name="password"]').click().type(userData.password);
+    cy.get('[data-qa="login-button"]').first().click();
+//   });
 });
 
 Then('I should see the dashboard page', () => {
   cy.url().should('include', '/dashboard'); // Adjust the path to your dashboard page
 });
-
-
-   /*
-   Given("Automation exercise page is opened", () => {
-  cy.visit(Cypress.env("baseURL"));
-  cy.wait(2000);
-});
-When("Verify it is the home page or not", () => {
-  cy.get("h1").should("contain.text", "AutomationExercise");
-});
-Then("Click on the Signup Login button", () => {
-  cy.get('[href="/login"]').click();
-});
-When("Verify that home page is visible successfully", () => {
-  cy.get("h2").eq(0).should("have.text", "Login to your account");
-});
-Then("Enter correct email address and password", () => {
-  cy.get('[data-qa="login-email"]').type(Cypress.env("userEmail"));
-  cy.get('[data-qa="login-password"]').type(Cypress.env("userPassword"));
-});
-Then("Click 'login' button", () => {
-  cy.get('[data-qa="login-button"]').click();
-});
-When("Verify that 'Logged in as username' is visible", () => {
-  cy.get("[class='nav navbar-nav'] > li:nth-child(10) > a > b").should(
-    "have.text",
-    "Zenia Banerjee"
-  );
-   */
-
 
 
